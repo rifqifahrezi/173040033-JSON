4b5195bb2314ebed7369f911dccf8d7aeb0e9c15
rifqifahrezi <55418191+rifqifahrezi@users.noreply.github.com>
2020-11-22 23:49:55 +0700

    Add files via upload

---
+++ 173040033/latihan1.php
@@ -0,0 +1,17 @@
+<?php
+
+$mahasiswa = [
+    [
+        "nama" => "Shandika Galih",
+        "nrp" => "043040023",
+        "email" => "sandhikagalih@unpas.ac.id"
+    ],
+    [
+        "nama" => "Erik Doank",
+        "nrp" => "023040001",
+        "email" => "erik@gmail.com"
+    ]
+];
+
+$data = json_encode($mahasiswa);
+echo $data;
